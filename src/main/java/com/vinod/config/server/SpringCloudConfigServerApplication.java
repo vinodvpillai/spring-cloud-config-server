7e17e5b4737aecbd73d8b06b9b5bae6dda1c0f8a
Vinod Pillai <vinodthebest@gmail.com>
2020-09-28 21:33:55 +0530

    spring-config-server: Added Git URL
    - Enabled git config server.
    - Added the git URL.

--- src/main/java/com/vinod/config/server/SpringCloudConfigServerApplication.java
+++ src/main/java/com/vinod/config/server/SpringCloudConfigServerApplication.java
@@ -2,8 +2,15 @@ package com.vinod.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Endpoint: http://localhost:8888/<file-name>/<profile>
+ * Example:
+ * http://localhost:8888/application/default
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
